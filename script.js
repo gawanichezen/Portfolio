82102aa2919a031b7cce018666384854288a0dee
gawanichezen <chezenanaydos@gmail.com>
2025-11-20 22:59:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const navbarLinks = document.querySelectorAll('.navbar a');
 
+
 // add click event to each link
 navbarLinks.forEach(link => {
     link.addEventListener('click', () => {
@@ -10,3 +11,24 @@ navbarLinks.forEach(link => {
         link.classList.add('active');
     });
 });
+
+
+// for resume part
+
+const resumeLists = document.querySelectorAll('.resume-list');
+const resumeBoxs = document.querySelectorAll('.resume-box');
+
+//resume section when clicking tab-list
+resumeLists.forEach((list, idx) => {
+    list.addEventListener('click', () => {
+        document.querySelector('.resume-list.active').classList.remove('active');
+        list.classList.add('active');
+
+        document.querySelector('.resume-box.active').classList.remove('active');
+        resumeBoxs[idx].classList.add('active');
+
+    });
+});
+
+
+ 
